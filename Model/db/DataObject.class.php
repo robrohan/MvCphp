3d4f10ac5d3480b7fc6c50e84fcf2519f3aaba46
robrohan <robrohan@891b9558-1820-0410-a9db-95fa21e03e57>
2006-11-22 02:08:23 +0000

    git-svn-id: https://svn9.cvsdude.com/robrohan/MvCphp/trunk@68 891b9558-1820-0410-a9db-95fa21e03e57

--- Model/db/DataObject.class.php
+++ Model/db/DataObject.class.php
@@ -31,25 +31,46 @@
 			}
 		}
 		
+		/**
+		 * For inserts and updates. Attempts to keep nasties from messing up SQL queries
+		 */
 		function CleanEntry($strValue) {
 			if(!empty($strValue)){
 				return addslashes($strValue);
 			}
 		}
 		
+		/**
+		 * Get an item by the id field
+		 */
 		function Get($id=1) {
 			$qry = "select * from " . get_class($this) . " where id=" . $this->CleanEntry($id);
 			$rslt = $this->layer->GetQuery($qry);
-			
+			__ResultSetToAttributes($rslt);
+		}
+		
+		/**
+		 * Get an item using and SQL fragment. The SQL parameter should start from after the from 
+		 * clause. So a valid paramenter for this method could be "where x = 12 and z = 34", or
+		 * "join x on b.id = x.id where z = 12"
+		 *
+		 * Note:
+		 * 	this doesn't protect you from writing something that returns more than one record. in
+		 * 	that event this object will hold the last item in the result set
+		 */
+		function FindByQuery($fragment) {
+			$qry = "select * from " . get_class($this) . $fragment;
+			$rslt = $this->layer->GetQuery($qry);
+			__ResultSetToAttributes($rslt);
+		}
+		
+		
+		function __ResultSetToAttributes($rslt) {
 			foreach($rslt as $row){
 				foreach($row as $name=>$value) {
 					$this->$name = $value;
-					//array_push($this,$name,$field);
 				}
 			}
-			
-			return $rslt;
 		}
-		
 	}
 ?>
