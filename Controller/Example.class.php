e584fca6857ea3ab884e873054428129021914bb
robrohan <robrohan@891b9558-1820-0410-a9db-95fa21e03e57>
2006-11-22 00:34:02 +0000

    reflection is not quite working
    git-svn-id: https://svn9.cvsdude.com/robrohan/MvCphp/trunk@40 891b9558-1820-0410-a9db-95fa21e03e57

--- Controller/Example.class.php
+++ Controller/Example.class.php
@@ -1,10 +1,12 @@
 <?php 
 	class Example {
-		function DoIt(){
+		function DoIt() {
+			$GLOBALS["Utils"]->Trace("Example::DoIt: Calling Example Controller");
 			$GLOBALS["TEST"] = "hello";
 		}
 			
 		function DoItAgain(){
+			$GLOBALS["Utils"]->Trace("Example::DoItAgain: Calling Example Controller");
 			$GLOBALS["TEST"] = "hello there";
 		}
 	}
