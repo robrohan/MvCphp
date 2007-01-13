061ec3d479328d69f9f8b7ebd286bb67858c390e
robrohan <robrohan@891b9558-1820-0410-a9db-95fa21e03e57>
2007-01-13 00:17:55 +0000

    git-svn-id: https://svn9.cvsdude.com/robrohan/MvCphp/trunk@121 891b9558-1820-0410-a9db-95fa21e03e57

--- View/About.php
+++ View/About.php
@@ -1,1 +1,5 @@
+
+<div style="text-align: center">
 <p>Copyright 2006-2007 Rob Rohan (rob.rohan@yahoo.com)</p>
+<p>Licensed Under the GPL</p>
+</div>
--- View/GettingStarted.php
+++ View/GettingStarted.php
@@ -1,9 +1,12 @@
 <p>Welcome to the MvCphp Framework.</p>
 
-<p>To get started you might want to read this post on how the whole framework goes
-together.</p>
+<p>To get started you might want to read 
+<a href="http://thoth.robrohan.com/client/index.cfm/2006/11/9/Simple-MVC-PHP-Framework">this</a> 
+post on how the whole framework goes
+together (<a href="http://thoth.robrohan.com/client/index.cfm/2007/1/9/Simple-MVC-PHP-Framework-15">here</a>
+is a follow up posting as well).</p>
 
-<p>To get started you'll probably want to edit the file AppCore/Settings.php. This
+<p>To get started you'll probably want to edit the file <i>AppCore/Settings.php</i>. This
 file contains all the framework settings. The ones you'll likely want to mess with
 are the follow:</p>
 
@@ -11,3 +14,11 @@ are the follow:</p>
 $USING_REWRITE - if you want SEF URLs and have mod_rewrite turned on.<br />
 $DB_USER, $DB_PASS, $DB_NAME, and $DB_HOST for database access. <br />
 </p>
+
+<p>And later on you'll probably want to change the following:</p>
+
+<p class="code">
+$APP_DEBUG - To turn off all the debug output, and trancing<br />
+$APP_STATE - To toggle settings between servers<br />
+</p>
+
