000191c02382bb040d0839740cae3745af1ded50
robrohan <robrohan@891b9558-1820-0410-a9db-95fa21e03e57>
2007-04-11 23:15:28 +0000

    The instantiation part of the code needed to take into account subdirectories as well
    git-svn-id: https://svn9.cvsdude.com/robrohan/MvCphp/trunk@148 891b9558-1820-0410-a9db-95fa21e03e57

--- index.php
+++ index.php
@@ -13,14 +13,24 @@
 	if($Utils->FileExists($ControllerFile)) {
 		include_once($ControllerFile);
 		
-		if( class_exists($GLOBALS['CONTROLLER']) ) {
-			$controller_obj = new $GLOBALS['CONTROLLER']();
+		$class_name =  $GLOBALS['CONTROLLER'];
+		$method_name = $GLOBALS['METHOD'];
+		
+		//if the controller is in a sub direcotry, the controller variable
+		//might have a / in it so we need to get that out
+		if( strpos($GLOBALS['CONTROLLER']) >= 0 ) {
+			$cparts = explode('/',$GLOBALS['CONTROLLER']);
+			$class_name = $cparts[(count($cparts)-1)];
+		}
+		
+		if( class_exists($class_name) ) {
+			$controller_obj = new $class_name();
 			
-			if( method_exists($controller_obj, $GLOBALS['METHOD']) ){
-				$Utils->Trace('Calling Controller: ' . $GLOBALS['CONTROLLER'] . '::' . $GLOBALS['METHOD']);
-				call_user_func( array($controller_obj, $GLOBALS['METHOD']) );
+			if( method_exists($controller_obj, $method_name) ){
+				$Utils->Trace('Calling Controller: ' . $class_name . '::' . $method_name);
+				call_user_func( array($controller_obj, $method_name) );
 			} else {
-				$Utils->AddError('Method "' . $GLOBALS['METHOD'] . '" is not defined on Controller "' . $GLOBALS['CONTROLLER'] . '" (' . $ControllerFile . ')');
+				$Utils->AddError('Method "' . $method_name . '" is not defined on Controller "' . $class_name . '" (' . $ControllerFile . ')');
 				$GLOBALS['VIEW'] = $GLOBALS['ERROR_VIEW'];
 			}
 		}
