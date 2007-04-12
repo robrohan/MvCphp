b7f39e806f960d290b022d02f96bc54c5ade6a42
robrohan <robrohan@891b9558-1820-0410-a9db-95fa21e03e57>
2007-04-12 00:16:13 +0000

    Changing some quotes
    git-svn-id: https://svn9.cvsdude.com/robrohan/MvCphp/trunk@162 891b9558-1820-0410-a9db-95fa21e03e57

--- View/MainView.php
+++ View/MainView.php
@@ -1,6 +1,6 @@
 <?php 
 	global $Utils;
-	$Utils->Trace("View::MainView: Showing main view"); 
+	$Utils->Trace('View::MainView: Showing main view'); 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html lang="en">
@@ -8,24 +8,21 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>MvC PHP Example Page</title>
 	<link rel="stylesheet" type="text/css" media="screen" 
-		href="<?= $GLOBALS["INSTALL_PATH"] ?>/View/Style/Default.css" />
+		href="<?= $GLOBALS['INSTALL_PATH'] ?>/View/Style/Default.css" />
 		
-	<script type="text/javascript" 
-		src="<?= $GLOBALS["INSTALL_PATH"] ?>/View/Javascript/Sortie.js"></script>
-	<script type="text/javascript" 
-		src="<?= $GLOBALS["INSTALL_PATH"] ?>/View/Javascript/json.js"></script>
-	<script type="text/javascript" 
-		src="<?= $GLOBALS["INSTALL_PATH"] ?>/View/Javascript/jsval.js"></script>
+	<script type="text/javascript" src="<?= $GLOBALS['INSTALL_PATH'] ?>/View/Javascript/Sortie.js"></script>
+	<script type="text/javascript" src="<?= $GLOBALS['INSTALL_PATH'] ?>/View/Javascript/json.js"></script>
+	<script type="text/javascript" src="<?= $GLOBALS['INSTALL_PATH'] ?>/View/Javascript/jsval.js"></script>
 </head>
 <body>
 	<div id="contents">
-		<h1><?php print( $GLOBALS["STRING.MAIN.TITLE"] ); ?></h1>
+		<h1><?php print( $GLOBALS['STRING.MAIN.TITLE'] ); ?></h1>
 	
-		<p><?php $Utils->ShowView($GLOBALS["PATH.MAIN.VIEW"]) ?></p>
+		<p><?php $Utils->ShowView($GLOBALS['PATH.MAIN.VIEW']) ?></p>
 		
 		<p>
-		<a href="<?= $Utils->CreateLink("Example",$GLOBALS["METHOD.LINK"]); ?>"><?= $GLOBALS["STRING.MAIN.LINK"] ?></a><br/>
-		<a href="<?= $Utils->CreateLink("Example","Remote"); ?>">JSON Example</a>
+		<a href="<?= $Utils->CreateLink('Example',$GLOBALS['METHOD.LINK']); ?>"><?= $GLOBALS['STRING.MAIN.LINK'] ?></a><br/>
+		<a href="<?= $Utils->CreateLink('Example','Remote'); ?>">JSON Example</a>
 		</p>
 	</div>
 </body>
